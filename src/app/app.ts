92c9d4648fa75b69b88ce2625b2e6bb35412e6a2
Andrea Manca <andrea@MacBook-Pro-de-Andrea.local>
2026-03-21 11:06:15 +0100

    sample

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ImageUploadComponent } from './components/image-upload/image-upload';
+import { ImageUpload } from './components/image-upload/image-upload';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ImageUploadComponent],
+  imports: [RouterOutlet, ImageUpload],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
